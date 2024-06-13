c361d6160858d0dd9f241022833987ae17d6bd0d
Jhonattas <jhonattas@soucriador.com>
2024-06-12 21:23:05 -0300

    envia para o server a index

---
+++ index.php
@@ -0,0 +1,14 @@
+<div class="tree">
+  <div class="header"><img src="https://pbs.twimg.com/profile_images/1361852374596395012/isM8He5__400x400.jpg" class="profile-pic" /></div>
+  <div class="username">@soucriador_</div>
+  <div class="links">
+    <a href="" class="btn btn-outline-primary btn-lg btn-block">Visit Our Website</a>
+    <a href="" class="btn btn-outline-success btn-lg btn-block">Shop Now and Save 40%</a>
+    <a href="" class="btn btn-outline-dark btn-lg btn-block">Learn How-to Build a Website using WordPress</a>
+    <a href="" class="btn btn-outline-primary btn-lg btn-block">Check Out Our Weekly Newsletter</a>
+    <a href="" class="btn btn-outline-primary btn-lg btn-block">Subscribe to Our YouTube Channel!</a>
+  </div>
+  <div class="footer">
+    Copyright &copy; Sou Criador - All Rights Reserved.
+  </div>
+</div>
